13f877c656f3fe69078ab60830b4fbfc83f05024
torreslucs23 <torreslucs23@gmail.com>
2024-09-04 23:21:32 -0300

    added more files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import { PrismaService } from './Database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private prisma: PrismaService) {}
 
   @Get()
   getHello(): string {
